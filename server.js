c877b1e6609847a01ca1d2b9c88ccc00b9082ab1
me <7647649476748i726782768793865norepel@github.com>
2022-10-04 21:13:29 +0800

    modified server.js in .

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
-var express = require("express");
-var app = express();
+var http = require("http"),
+  url = require("url"),
+  path = require("path"),
+  fs = require("fs");
+port = process.argv[2] || 8888;
 
-app.get("/", function (req, res) {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.end('/index.html');
-});
+http
+  .createServer(function (request, response) {
+    var uri = url.parse(request.url).pathname,
+      filename = path.join(process.cwd(), uri);
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
-});
-~
+    fs.exists(filename, function (exists) {
+      if (!exists) {
+        response.writeHead(404, { "Content-Type": "text/plain" });
+        response.write("404 Not Found\n");
+        response.end();
+        return;
+      }
+
+      if (fs.statSync(filename).isDirectory()) filename += "/index.html";
+
+      fs.readFile(filename, "binary", function (err, file) {
+        if (err) {
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.write(err + "\n");
+          response.end();
+          return;
+        }
+
+        response.writeHead(200);
+        response.write(file, "binary");
+        response.end();
+      });
+    });
+  })
+  .listen(parseInt(port, 10));
+
+console.log(
+  "Static file server running at\n  => http://localhost:" +
+    port +
+    "/\nCTRL + C to shutdown"
+);
